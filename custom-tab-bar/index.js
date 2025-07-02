f3a6942e892eaebaa78414a039838b4eb6d4bdb3
HandyWote <18666119673@163.com>
2025-07-03 00:25:52 +0800

    删除设置页面及相关文件，更新应用配置，调整库版本至3.8.9

--- custom-tab-bar/index.js
+++ custom-tab-bar/index.js
@@ -31,8 +31,7 @@ Component({
          [
           { "pagePath": "/pages/home/home","iconSize":48, "iconPath" :"/images/tabs/home.png" ,"text": "首页" },
           { "pagePath": "/pages/ai-qa/ai-qa","iconSize":48,"iconPath" :"/images/tabs/AI.png", "text": "AI-问答" },
-          { "pagePath": "/pages/onlineClass/onlineClass","iconSize":48,"iconPath" :"/images/tabs/onlineClass.png",  "text": "在线课堂" },
-          { "pagePath": "/pages/settings/settings","iconSize":48, "iconPath" :"/images/tabs/setting.png", "text": "设置" }
+          { "pagePath": "/pages/onlineClass/onlineClass","iconSize":48,"iconPath" :"/images/tabs/onlineClass.png",  "text": "在线课堂" }
         ]
   },
 
